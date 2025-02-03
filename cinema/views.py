196a516b98b56db6e73c2f31ecc52e1411f96263
fanni <fmalevych@zoolatech.com>
2025-02-03 15:45:48 +0200

    fixes

--- cinema/views.py
+++ cinema/views.py
@@ -40,7 +40,11 @@ class GenreDetail(views.APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
-        serializer = GenreSerializer(self.get_object(pk=pk), data=request.data, partial=True)
+        serializer = GenreSerializer(
+            self.get_object(pk=pk),
+            data=request.data,
+            partial=True
+        )
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
