859fa2de0d32492b6c61f56a1c21bdc250c2b15a
fanni <fmalevych@zoolatech.com>
2025-02-03 15:17:18 +0200

    add cinema hall view

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail, genre_list, genre_detail, ActorDetail, ActorList
+from cinema.views import movie_list, movie_detail, genre_list, genre_detail, ActorDetail, ActorList, CinemaHallViewSet
+
+cinema_hall_list = CinemaHallViewSet.as_view(actions={"get": "list", "post": "create"})
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
@@ -9,6 +13,8 @@ urlpatterns = [
     path("genres/<int:pk>/", genre_detail, name="genre_detail"),
     path("actors/", ActorList.as_view(), name="genre-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor_detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema_hall_list"),
+    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema_hall_detail"),
 ]
 
 app_name = "cinema"
